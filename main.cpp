7d7b15d5e6d1d2d14cf3f9f313dc83bfb078f105
JRoy-beniola <royjayata1@gmail.com>
2025-04-07 17:35:04 +0530

    First object and the flag of Japan!

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 
 color ray_color(const ray&);
+bool sphere_hit(const point3&, double, const ray&);
 
 
 int main()
@@ -70,7 +71,28 @@ int main()
 
 color ray_color(const ray& r)
 {
+    if (sphere_hit(point3(0, 0, -1), 0.5, r))
+    {
+        return color(1.0, 0.0, 0.0);
+    }
+    else
+    {
+        return color(1.0, 1.0, 1.0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto alpha = 0.5 * (unit_direction.y() + 1.0);
     return (1 - alpha)*color(1.0, 1.0, 1.0) + alpha*color(0.5, 0.7, 1.0);
+}
+
+bool sphere_hit(const point3& center, double radius, const ray& r)
+{
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+
+    auto discriminant = b * b - 4 * a * c;
+
+    return (discriminant >= 0);
 }
